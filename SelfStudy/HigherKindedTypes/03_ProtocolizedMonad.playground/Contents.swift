b7959cd53c7755b48353a41005328a762cf72a4a
Heecheon Park <heecheon92@gmail.com>
2023-08-02 21:57:07 +0900

    (HKT Monad) added a study case for higher-kinded type monad protocol

---
+++ SelfStudy/HigherKindedTypes/03_ProtocolizedMonad.playground/Contents.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+// MARK: - Functor Declaration
+protocol Functor {
+    associatedtype T
+    associatedtype U: Functor = Self
+    
+    func map<V>(_ transform: ((T) -> V)) -> U where U.T == V
+}
+
+protocol Monad: Functor {
+    associatedtype M: Functor = Self
+    
+    func flatMap<V>(_ transform: ((T) -> M)) -> U where U.T == V
+}
+
+struct MayBe<T>: Monad {
+    let value: T
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func map<V>(_ transform: ((T) -> V)) -> MayBe<V> {
+        return MayBe<V>(transform(value))
+    }
+    
+    func apply<V>(_ boxedThunk: MayBe<(T) -> V>) -> MayBe<V> {
+        return MayBe<V>(boxedThunk.value(value))
+    }
+    
+    func flatMap<V>(_ transform: ((T) -> MayBe<V>)) -> MayBe<V> {
+        return transform(value)
+    }
+    
+    func flatMap<V>(_ transform: ((T) -> V)) -> MayBe<V> {
+        return MayBe<V>(transform(value))
+    }
+}
+
+extension MayBe: CustomStringConvertible {
+    var description: String { "\(type(of: value))" }
+}
+
+let fmaybeInt                  = MayBe(1)
+let fmaybeString               = fmaybeInt.map { "\($0)" }
+let fmaybeMaybeString          = fmaybeString.map { MayBe($0) }
+let fmaybeOptionalString       = fmaybeString.map { Optional<String>($0) }
+let fmaybeMaybeOptionalString  = fmaybeMaybeString.map { $0.map({ v in Optional(v) }) }
+let fmaybeOptionalMaybeString  = fmaybeOptionalString.map { $0.map({ v in MayBe(v) }) }
+
+let mmaybeInt                  = MayBe(1)
+let mmaybeString               = mmaybeInt.flatMap { MayBe("\($0)") }
+let mmaybeMaybeString          = mmaybeString.flatMap { MayBe($0) }
+let mmaybeOptionalString       = mmaybeString.flatMap { Optional<String>($0) }
+let mmaybeMaybeOptionalString  = mmaybeMaybeString.flatMap { $0.flatMap({ v in Optional(v) }) }
+let mmaybeOptionalMaybeString  = mmaybeOptionalString.flatMap { $0.flatMap({ v in MayBe(v) }) }
+
+print(fmaybeInt)
+print(fmaybeString)
+print(fmaybeMaybeString)
+print(fmaybeOptionalString)
+print(fmaybeMaybeOptionalString)
+print(fmaybeOptionalMaybeString)
+print("===================================")
+print(mmaybeInt)
+print(mmaybeString)
+print(mmaybeMaybeString)
+print(mmaybeOptionalString)
+print(mmaybeMaybeOptionalString)
+print(mmaybeOptionalMaybeString)
