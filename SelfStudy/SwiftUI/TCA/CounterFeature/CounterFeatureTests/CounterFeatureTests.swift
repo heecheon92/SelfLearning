0c07c4ba4e75a07c8ee93eed35b87fe41b4c2733
Heecheon Park <heecheon@thesesamelab.com>
2025-09-26 09:50:10 +0900

    learning how to inject external dependency to the store

--- SelfStudy/SwiftUI/TCA/CounterFeature/CounterFeatureTests/CounterFeatureTests.swift
+++ SelfStudy/SwiftUI/TCA/CounterFeature/CounterFeatureTests/CounterFeatureTests.swift
@@ -33,14 +33,17 @@ import Testing
 
     @Test func numberFact() async {
         let store = TestStore(
-            initialState: CounterFeature.State(), reducer: { CounterFeature() })
-        
+            initialState: CounterFeature.State(), reducer: { CounterFeature() },
+            withDependencies: {
+                $0.numberFact.fetch = { "\($0) is a good number." }
+            })
+
         await store.send(.factButtonTapped) {
             $0.isLoading = true
         }
-        await store.receive(\.factResponse, timeout: .seconds(1)) {
+        await store.receive(\.factResponse) {
             $0.isLoading = false
-            $0.fact = "???"
+            $0.fact = "0 is a good number."
         }
     }
 }
