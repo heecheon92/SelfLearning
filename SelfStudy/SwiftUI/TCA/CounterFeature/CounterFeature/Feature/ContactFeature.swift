ee20ad229f250a058db06b0e92747a5b9674ed0f
Heecheon Park <heecheon@thesesamelab.com>
2025-09-30 13:50:28 +0900

    refactoring presentation logic such that a single presentation can be conducted at a time

--- SelfStudy/SwiftUI/TCA/CounterFeature/CounterFeature/Feature/ContactFeature.swift
+++ SelfStudy/SwiftUI/TCA/CounterFeature/CounterFeature/Feature/ContactFeature.swift
@@ -17,15 +17,13 @@ struct Contact: Equatable, Identifiable {
 @Reducer struct ContactFeature {
 
     @ObservableState struct State: Equatable {
-        @Presents var addContact: AddContactFeature.State?
-        @Presents var alert: AlertState<Action.Alert>?
         var contacts: IdentifiedArrayOf<Contact> = []
+        @Presents var destination: Destination.State?
     }
 
     enum Action {
         case addButtonTapped
-        case addContact(PresentationAction<AddContactFeature.Action>)
-        case alert(PresentationAction<Alert>)
+        case destination(PresentationAction<Destination.Action>)
         case deleteButtonTapped(Contact.ID)
         enum Alert: Equatable {
             case confirmDeletion(id: Contact.ID)
@@ -36,41 +34,55 @@ struct Contact: Equatable, Identifiable {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
-                state.addContact = AddContactFeature.State(
-                    contact: .init(id: UUID(), name: "Tester"))
+                state.destination = .addContact(
+                    AddContactFeature.State(
+                        contact: Contact(id: UUID(), name: "Tester")
+                    )
+                )
                 return .none
 
-            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+            case let .destination(
+                .presented(.addContact(.delegate(.saveContact(contact))))):
                 state.contacts.append(contact)
                 return .none
 
-            case .addContact:
-                return .none
-
-            case let .deleteButtonTapped(id):
-                state.alert = AlertState {
-                    TextState("Are you sure?")
-                } actions: {
-                    ButtonState(
-                        role: .destructive, action: .confirmDeletion(id: id)
-                    ) {
-                        TextState("Delete")
-                    }
-                }
-                return .none
-            case let .alert(.presented(.confirmDeletion(id))):
+            case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
                 state.contacts.remove(id: id)
                 return .none
-            case .alert: return .none
-            }
 
+            case .destination:
+                return .none
+
+            case let .deleteButtonTapped(id: id):
+                state.destination = .alert(
+                    AlertState {
+                        TextState("Are you sure?")
+                    } actions: {
+                        ButtonState(
+                            role: .destructive, action: .confirmDeletion(id: id)
+                        ) {
+                            TextState("Delete")
+                        }
+                    }
+                )
+                return .none
+            }
         }
-        .ifLet(\.$addContact, action: \.addContact) {
-            AddContactFeature()
+        .ifLet(\.$destination, action: \.destination) {
+            Destination.body
         }
-        .ifLet(\.$alert, action: \.alert)
     }
 }
+
+extension ContactFeature {
+    @Reducer
+    enum Destination {
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactFeature.Action.Alert>)
+    }
+}
+
+extension ContactFeature.Destination.State: Equatable {}
 
 struct ContactView: View {
     @Bindable var store: StoreOf<ContactFeature>
@@ -104,15 +116,15 @@ struct ContactView: View {
         }
         .sheet(
             item: $store.scope(
-                state: \.addContact,
-                action: \.addContact)
+                state: \.destination?.addContact,
+                action: \.destination.addContact)
         ) {
             addContactStore in
             NavigationStack {
                 AddContactView(store: addContactStore)
             }
         }
-        .alert($store.scope(state: \.alert, action: \.alert))
+        .alert($store.scope(state: \.destination?.alert, action: \.destination.alert))
     }
 }
 
